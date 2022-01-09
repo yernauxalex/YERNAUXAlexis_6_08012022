7a41be17e49de32d8f9f38da6e5eb9f5506ef8bd
Alexis <yernauxalex@gmail.com>
2022-01-09 09:54:55 +0100

    Ajout CORS

--- backend/app.js
+++ backend/app.js
@@ -5,16 +5,27 @@ const mongoose = require('mongoose');
 const dotenv = require("dotenv");
 dotenv.config();
 
-// Récupération des logs de la DB
-const log = require ('./log_DB');
-
-// mongoDB
+// MongoDB
 mongoose.connect(`${process.env.MONGOLOG}`, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	})
 	.then(() => console.log('Connected to MongoDB'))
 	.catch(() => console.log('Connection failed'));
+
+// CORS
+app.use((req, res, next) => {
+	// Accéder à l'API depuis n'importe où
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	// Ajouter les headers sur nos réponses
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+	)
+	// Permet d'utiliser le CRUD
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+	next()
+})
 
 
 
