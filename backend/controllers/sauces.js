11ceb8fc2e4af120ad9ea5d2ef402e5501ebd124
Alexis <yernauxalex@gmail.com>
2022-01-13 14:34:28 +0100

    Fix fs

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -1,13 +1,14 @@
 const Sauce = require('../models/Sauce');
-const file = require('file-system');
+const fs = require('fs');
 
+// Création d'une nouvelle sauce dans la DB
 exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);
     // Crée une instance de sauce selon son model
     const sauce = new Sauce({
 		...sauceObject,
 		imageUrl: `${req.protocol}://${req.get('host')}/images/${
-			req.file.filename
+			req.fs.filename
 		}`,
 	});
     sauce
@@ -16,18 +17,22 @@ exports.createSauce = (req, res, next) => {
         .catch((error) => res.status(400).json({message: error}))
 };
 
+// Recherche d'une sauce dans la DB
 exports.getOneSauce = (req, res, next) => {
 
 };
 
+// Modification d'une sauce dans la DB
 exports.modifySauce = (req, res, next) => {
 
 };
 
+// Suppression d'une sauce dans la DB
 exports.deleteSauce = (req, res, next) => {
 
 };
 
+// Récupération de toutes les sauces présentes dans la DB
 exports.getAllSauces = (req, res, next) => {
 
 };
