e4c8d75856daef4ac5506a5d89af92aafa06bd22
Alexis <yernauxalex@gmail.com>
2022-01-14 13:48:54 +0100

    WIP sauces controllers

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 // Création d'une nouvelle sauce dans la DB
 exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);
+    delete sauceObject._id;
     // Crée une instance de sauce selon son model
     const sauce = new Sauce({
 		...sauceObject,
@@ -14,17 +15,36 @@ exports.createSauce = (req, res, next) => {
     sauce
         .save()
         .then(() => res.status(201).json({message: 'Sauce enregistrée'}))
-        .catch((error) => res.status(400).json({message: error}))
+        .catch((error) => res.status(400).json({message: error}));
 };
 
 // Recherche d'une sauce dans la DB
 exports.getOneSauce = (req, res, next) => {
-
+    Sauce.findOne({ _id: req.params.id }) 
+		.then((sauce) => {
+			res.status(200).json(sauce)
+		})
+		.catch((error) => {
+			res.status(404).json({ error })
+		});
 };
 
 // Modification d'une sauce dans la DB
 exports.modifySauce = (req, res, next) => {
-
+    const sauceObject = req.file
+		? {
+				...JSON.parse(req.body.sauce),
+				imageUrl: `${req.protocol}://${req.get('host')}/images/${
+					req.file.filename
+				}`,
+		  }
+		: { ...req.body }
+	Sauce.updateOne(
+		{ _id: req.params.id },
+		{ ...sauceObject, _id: req.params.id }
+	)
+		.then(() => res.status(200).json({ message: 'Sauce modifiée' }))
+		.catch((error) => res.status(400).json({ error }));
 };
 
 // Suppression d'une sauce dans la DB
